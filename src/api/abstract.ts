5265299b11ea64dff176eef97a213d227873b3a2
孙伟杰 <weijie.sun@yitu-inc.com>
2022-11-23 10:08:00 +0800

    fix: add appid

--- src/api/abstract.ts
+++ src/api/abstract.ts
@@ -10,7 +10,7 @@ import i18n from '@/plugin/i18n';
 import storage from '@/utils/storage';
 
 class Abstract {
-  protected baseURL = 'https://demo-bapi-digizen.lianwei.com.cn/';
+  protected baseURL = '';
 
   protected timeout = 60000;
 
